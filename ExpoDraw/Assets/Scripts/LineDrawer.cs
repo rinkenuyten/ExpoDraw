b69d07ffd3029ee5e0e3863cfd4459474c8077c3
Rinke <rinke.nuyten@hotmail.com>
2015-12-10 13:21:10 +0100

    Experiment #1

--- ExpoDraw/Assets/Scripts/LineDrawer.cs
+++ ExpoDraw/Assets/Scripts/LineDrawer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LineDrawer : MonoBehaviour
 {
@@ -9,54 +10,58 @@ public class LineDrawer : MonoBehaviour
     private Vector3 mousePos;
     //assign a material to the Line Renderer in the Inspector
     public Material material;
+
     //number of lines drawn
-    private int currLines = 0;
+    private List<LineRenderer> lines = new List<LineRenderer>();
+
+
+
+    private Vector2 fingerStart;
+    private Vector2 fingerEnd;
 
     void Update()
     {
-        //Create new Line on left mouse click(down)
-        if (Input.GetMouseButtonDown(0))
-        {
-            //check if there is no line renderer created
-            if (line == null)
+        //////////////////////////////////////////////////////////////////
+        // Android swipe input
+            foreach (Touch touch in Input.touches)
             {
-                //create the line
-                createLine();
+                if (touch.phase == TouchPhase.Began)
+                {
+                    fingerStart = touch.position;
+                    fingerEnd = touch.position;
+                }
+
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    fingerEnd = touch.position;
+                    if (Vector2.Distance(fingerStart, fingerEnd) > 0.01f)
+                    {
+                        if (line == null)
+                        {
+                            createLine();
+
+                            line.SetPosition(0, fingerStart);
+                            line.SetPosition(1, fingerEnd);
+
+                            fingerStart = fingerEnd;
+                            fingerEnd = Vector2.zero;
+                        }
+                    }
+                }
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    fingerStart = Vector2.zero;
+                    fingerEnd = Vector2.zero;
+                }
             }
-            //get the mouse position
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //set the z co ordinate to 0 as we are only interested in the xy axes
-            mousePos.z = 0;
-            //set the start point and end point of the line renderer
-            line.SetPosition(0, mousePos);
-            line.SetPosition(1, mousePos);
-        }
-        //if line renderer exists and left mouse button is click exited (up)
-        else if (Input.GetMouseButtonUp(0) && line)
-        {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0;
-            //set the end point of the line renderer to current mouse position
-            line.SetPosition(1, mousePos);
-            //set line as null once the line is created
-            line = null;
-            currLines++;
-        }
-        //if mouse button is held clicked and line exists
-        else if (Input.GetMouseButton(0) && line)
-        {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0;
-            //set the end position as current position but dont set line as null as the mouse click is not exited
-            line.SetPosition(1, mousePos);
-        }
     }
 
     //method to create line
     private void createLine()
     {
         //create a new empty gameobject and line renderer component
-        line = new GameObject("Line" + currLines).AddComponent<LineRenderer>();
+        line = new GameObject("Line").AddComponent<LineRenderer>();
         //assign the material to the line
         line.material = material;
         //set the number of points to the line
@@ -65,6 +70,6 @@ public class LineDrawer : MonoBehaviour
         line.SetWidth(0.15f, 0.15f);
         //render line to the world origin and not to the object's position
         line.useWorldSpace = true;
-
+        lines.Add(line);
     }
 }
