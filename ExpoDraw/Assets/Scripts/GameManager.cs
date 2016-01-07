0030a309cdec0bcada658d3f21b41686ba6e0ca3
KevinvdBurg <vanderburg.kevin@gmail.com>
2016-01-07 10:30:29 +0100

    Added all Opdrachten

--- ExpoDraw/Assets/Scripts/GameManager.cs
+++ ExpoDraw/Assets/Scripts/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour {
     public Texture texture1;
     public Texture texture2;
 	public Texture texture3;
+	public Texture texture4;
+	public Texture texture5;
 
 	void Start () 
     {
@@ -32,10 +34,11 @@ public class GameManager : MonoBehaviour {
         
 		//--------------------
 
-	    for (int i = 5; i < 10; i++)
-	    {
-	        newInts.Add(i);
-	    }
+		newInts.Add(1);
+		newInts.Add(4);
+		newInts.Add(6);
+		newInts.Add(9);
+		newInts.Add(12);
         
 		newBrush.Add(texture1);
 
@@ -54,10 +57,11 @@ public class GameManager : MonoBehaviour {
 		newBrush = new List<Texture> ();
 		newColor = new List<Color> ();
 
-		for (int i = 4; i < 9; i++)
-		{
-			newInts.Add(i);
-		}
+		newInts.Add(1);
+		newInts.Add(4);
+		newInts.Add(6);
+		newInts.Add(9);
+		newInts.Add(12);
 
 		newBrush.Add(texture1);
 
@@ -84,7 +88,7 @@ public class GameManager : MonoBehaviour {
 		newColor = new List<Color> ();
 
 		newInts.Add(6);
-		newInts.Add(12);
+		newInts.Add(9);
 
 		newBrush.Add(texture1);
 		newBrush.Add(texture2);
@@ -104,21 +108,155 @@ public class GameManager : MonoBehaviour {
 
 		newBrush.Add(texture1);
 		newBrush.Add(texture2);
+		newBrush.Add(texture3);
 
-		newColor.Add(Color.blue);
+		newColor.Add(Color.black);
+		newColor.Add(Color.white);
+		newColor.Add(Color.gray);
+
+		Opdrachten.Add(new Opdracht("Tekentechniek: Clair-Obscur", "Da Vinci heeft 2 verschillende technieken ontworpen. Een hiervan heet: “Clair-Obscur”\nDit is een techniek waarbij de vorm gesuggereerd wordt door het gebruik van licht en donker. Deze contrasten tussen licht en donker worden sterker weergegeven dan ze in werkelijkheid zijn. Hierbij worden de objecten op de achtergrond donker te kleuren en objecten op de voorgrond licht.  Dit is een hulpmiddel om werk realistisch te laten lijken.\nProbeer met behulp van deze techniek dit schilderij na te maken.", newInts, newBrush, newColor));
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(4);
+		newInts.Add(6);
+
+		newBrush.Add(texture1);
+
 		newColor.Add(Color.red);
+		newColor.Add(Color.blue);
 		newColor.Add(Color.yellow);
-		newColor.Add(Color.green);
 
-		Opdrachten.Add(new Opdracht("Emoties", "In dit kunstwerk worden de reacties van de apostelen weergegeven toen Jezus vertelde dat een van hen hem verraden zou hebben. De apostelen zijn in 4 groepjes verdeeld. Elke groep apostelen heeft een eigen emotie centraal staan. Neem 1 groepje en teken jouw interpretatie van hun reactie (emotie).", newInts, newBrush, newColor));
+		Opdrachten.Add(new Opdracht("Portret", "De meeste schilderijen rond deze tijd waren portret schilderijen. Doordat Da Vinci hier geen portret heeft geschilderd, maar juist een groep mensen, maakt dit zo bijzonder voor deze tijd. Neem één persoon uit dit schilderij en teken van deze persoon een portret.\n", newInts, newBrush, newColor));
+
 
 		Painting avondmaal = new Painting ("Het Laatste Avondmaal", Opdrachten);
-		paintings.Add (avondmaal);
+		paintings.Add(avondmaal);
+
+		//--------------------
+		// new painting
+
+		Opdrachten.Clear ();
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(6);
+		newInts.Add(9);
+
+		newBrush.Add(texture1);
+		newBrush.Add(texture2);
+
+		newColor.Add(Color.yellow);
+		newColor.Add(Color.blue);
+		newColor.Add(Color.red);
+		newColor.Add(Color.white);
+
+		Opdrachten.Add(new Opdracht("Tekenen op inspiratie", "De inspiratie van dit schilderij kwam van het stratenpatroon in New York en van de swingende jazzmuziek van die tijd. Teken jouw variant met de verkregen inspiratie.", newInts, newBrush, newColor));
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(4);
+		newInts.Add(6);
+		newInts.Add(9);
+
+		newBrush.Add(texture1);
+		newBrush.Add(texture2);
+		newBrush.Add(texture3);
+		newBrush.Add(texture4);
+		newBrush.Add(texture5);
+
+		newColor.Add(Color.yellow);
+
+		Opdrachten.Add(new Opdracht("Tekenen met één kleur", "Teken door middel van 1 kleur dit schilderij na.", newInts, newBrush, newColor));
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(4);
+		newInts.Add(6);
+		newInts.Add(9);
+
+		newBrush.Add(texture1);
+
+		newColor.Add(Color.yellow);
+		newColor.Add(Color.red);
+		newColor.Add(Color.blue);
+
+		Opdrachten.Add(new Opdracht("Wat zie jij?", "Mondriaan is een schilder die vooral abstracte kunstwerken maakt. Ook dit is weer een abstract kunstwerk. Teken wat jij ziet in dit kunstwerk met de kleuren die ook in dit kunstwerk verwerkt zijn.", newInts, newBrush, newColor));
+
+		Painting boogie = new Painting ("Broadway Boogie Woogie ", Opdrachten);
+		paintings.Add(boogie);
 
 
 		//--------------------
+		// new painting
 
+		Opdrachten.Clear ();
 
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(1);
+		newInts.Add(4);
+		newInts.Add(6);
+		newInts.Add(9);
+
+		newBrush.Add(texture1);
+		newBrush.Add(texture2);
+
+		newColor.Add(Color.yellow);
+		newColor.Add(Color.blue);
+		newColor.Add(Color.red);
+
+		Opdrachten.Add(new Opdracht("Ochtend naar middag", "Het schilderij is geschilderd net voordat de zon op komt. Maar hoe zou dit schilderij er uit zien als het in de middag werd geschilderd? Misschien is alles wel scherper aangezien het niet meer schemer", newInts, newBrush, newColor));
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(4);
+		newInts.Add(6);
+		newInts.Add(9);
+
+		newBrush.Add(texture1);
+
+		newColor.Add(Color.blue);
+		newColor.Add(Color.yellow);
+		newColor.Add(Color.black);
+		newColor.Add(Color.white);
+		newColor.Add(Color.cyan);
+
+		Opdrachten.Add(new Opdracht("Van verf naar potlood", "Van Gogh maakte dit schilderij met alleen olie verf.\nzorg er voor dat je die schilderij zo goed mogelijk natekenend met alleen potlood.", newInts, newBrush, newColor));
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(4);
+		newInts.Add(6);
+		newInts.Add(9);
+
+		newBrush.Add(texture1);
+
+		newColor.Add(Color.yellow);
+		newColor.Add(Color.red);
+		newColor.Add(Color.blue);
+		newColor.Add(Color.cyan);
+		newColor.Add(Color.black);
+		newColor.Add(Color.white);
+
+		Opdrachten.Add(new Opdracht("Tekenen met bogen", "In dit schilderij is het gebruik van bogen erg belangrijk. Probeer nu zelf dit schilderij na te tekenen zonder het gebruik van rechte lijnen.", newInts, newBrush, newColor));
+
+		Painting sterrennacht = new Painting ("Sterrennacht", Opdrachten);
+		paintings.Add(sterrennacht);
 
         SetOpdracht(Opdrachten[0]);
 	}
