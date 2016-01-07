b8fa3533095eebb815ca5105cc1067d9825f0928
KevinvdBurg <vanderburg.kevin@gmail.com>
2016-01-07 10:08:07 +0100

    Added Opdrachten

--- ExpoDraw/Assets/Scripts/GameManager.cs
+++ ExpoDraw/Assets/Scripts/GameManager.cs
@@ -5,10 +5,12 @@ using AssemblyCSharp;
 
 public class GameManager : MonoBehaviour {
     public List<Opdracht> Opdrachten;
+	public List<Painting> paintings;
     public Opdracht ActieveOpdracht;
     public Camera cam;
     public Texture texture1;
     public Texture texture2;
+	public Texture texture3;
 
 	void Start () 
     {
@@ -18,28 +20,102 @@ public class GameManager : MonoBehaviour {
 
 
 
+		List<int> newInts = new List<int>();
+		List<Texture> newBrush = new List<Texture>();
+		List<Color> newColor = new List<Color>();
+
+
+
         /////////////////////////////////
         ////// ADD OPDRACHTEN HERE///////
         /////////////////////////////////
         
 		//--------------------
 
-        List<int> newInts = new List<int>();
-	    for (int i = 5; i < 8; i++)
+	    for (int i = 5; i < 10; i++)
 	    {
 	        newInts.Add(i);
 	    }
+        
+		newBrush.Add(texture1);
 
-        List<Texture> newText = new List<Texture>();
-        newText.Add(texture1);
-
-        List<Color> newColor = new List<Color>();
+        
         newColor.Add(Color.red);
         newColor.Add(Color.blue);
+		newColor.Add(Color.yellow);
+		newColor.Add (Color.green);
 
-        Opdrachten.Add(new Opdracht("yolo", "Goede beschrijving", newInts, newText, newColor));
+		Opdrachten.Add(new Opdracht("Maak van Donker Licht", "Rembrandt heeft dit schilderij erg donker getekend om het realistischer over te laten komen. Teken dit schilderij met lichte kleuren.", newInts, newBrush, newColor));
+
+
+
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		for (int i = 4; i < 9; i++)
+		{
+			newInts.Add(i);
+		}
+
+		newBrush.Add(texture1);
+
+		newColor.Add(Color.red);
+		newColor.Add(Color.black);
+		newColor.Add(Color.white);
+		newColor.Add (Color.blue);
+		newColor.Add (Color.green);
+
+		Opdrachten.Add(new Opdracht("Teken symbolieken", "Volgens de geleerde heeft Rembrandt een aantal symbolieken toegevoegd aan dit schilderij. Een hiervan is “De uitgetrokken handschoen van de kapitein”.", newInts, newBrush, newColor));
+
 
 		Painting nachtwacht = new Painting ("Nachtwacht", Opdrachten);
+		paintings.Add (nachtwacht);
+
+		//--------------------
+		// new painting
+
+
+		Opdrachten.Clear ();
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(6);
+		newInts.Add(12);
+
+		newBrush.Add(texture1);
+		newBrush.Add(texture2);
+
+		newColor.Add(Color.blue);
+		newColor.Add(Color.red);
+		newColor.Add(Color.yellow);
+		newColor.Add(Color.green);
+
+		Opdrachten.Add(new Opdracht("Emoties", "In dit kunstwerk worden de reacties van de apostelen weergegeven toen Jezus vertelde dat een van hen hem verraden zou hebben. De apostelen zijn in 4 groepjes verdeeld. Elke groep apostelen heeft een eigen emotie centraal staan. Neem 1 groepje en teken jouw interpretatie van hun reactie (emotie).", newInts, newBrush, newColor));
+
+		newInts = new List<int>();
+		newBrush = new List<Texture> ();
+		newColor = new List<Color> ();
+
+		newInts.Add(4);
+
+		newBrush.Add(texture1);
+		newBrush.Add(texture2);
+
+		newColor.Add(Color.blue);
+		newColor.Add(Color.red);
+		newColor.Add(Color.yellow);
+		newColor.Add(Color.green);
+
+		Opdrachten.Add(new Opdracht("Emoties", "In dit kunstwerk worden de reacties van de apostelen weergegeven toen Jezus vertelde dat een van hen hem verraden zou hebben. De apostelen zijn in 4 groepjes verdeeld. Elke groep apostelen heeft een eigen emotie centraal staan. Neem 1 groepje en teken jouw interpretatie van hun reactie (emotie).", newInts, newBrush, newColor));
+
+		Painting avondmaal = new Painting ("Het Laatste Avondmaal", Opdrachten);
+		paintings.Add (avondmaal);
+
+
 		//--------------------
 
 
